5a53defe75997745c1773b698efc9d8c6fe88a4a
tommy <tommywhy1989@gmail.com>
2020-07-07 13:45:35 +1200

    algorithm

---
+++ algorithm-study/al-1/CMakeFiles/algorithm1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm1.dir/src/main.cpp.o"
+  "algorithm1"
+  "algorithm1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithm1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ algorithm-study/al-1/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+#if 1
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    vector<int>v;
+
+    return 0;
+}
+#endif
