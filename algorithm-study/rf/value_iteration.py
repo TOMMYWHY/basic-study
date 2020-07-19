536e654c82031495b23080a4968caa9175b5753f
tommy <tommywhy1989@gmail.com>
2020-07-19 16:49:51 +1200

    QLearning_demo

--- algorithm-study/rf/value_iteration.py
+++ algorithm-study/rf/value_iteration.py
@@ -9,4 +9,24 @@ def value_iteration(env,theta=0.0001,discount_factor=1.0):
         for a in range(env.nA):
             for prob,next_state,reward,done in env.P[state][a]:
                 A[a] += prob* (reward + discount_factor*v[next_state])
-                
+        return A
+    v=np.zeros(env.nS)
+    while True:
+        delta = 0
+        for s in range(env.nS):
+
+            A = one_step_lookahead(s,v)
+            best_action_value = np.max(A)
+            delta = max(delta,np.abs(best_action_value-v[s]))
+            v[s] = best_action_value
+        if delta < theta:
+            break
+
+    policy= np.zeros((env.nS,env.nA))
+    for s in range(env.nS):
+        A = one_step_lookahead(s,v)
+        best_action_value=np.max(A)
+        policy[s,best_action_value]=1.0
+    return policy,v
+
+policy,v = value_iteration(env)
